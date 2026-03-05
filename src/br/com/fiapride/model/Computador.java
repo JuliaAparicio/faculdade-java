f620600eb6f244ba9cc1181fb1035892e19fe563
Julia Aparicio <juliaaparicio756@gmail.com>
2026-03-04 21:10:23 -0300

    Update Computador.java

--- src/br/com/fiapride/model/Computador.java
+++ src/br/com/fiapride/model/Computador.java
@@ -1,7 +1,39 @@
 package br.com.fiapride.model;
 
 public class Computador {
-	public String tamanho;
-	public double memoria;
+	public String preco;
+	public String memoria;
+	public double tamanho;
 
+	public Computador(String preco, String memoria) {
+		this.preco = preco;
+		this.memoria = memoria
+		this.tamanho = 70.5
 }
+
+	public void adicionarSaldo(double valor) {
+		// Regra de negócio: O valor da recarga deve ser positivo
+		if (valor <= 0) {
+			System.out.printIn("Erro: O valor de recarga deve ser maior que zero");
+			
+			return; // Interrompe a execução do método
+		}
+ 		this.saldo += valor;
+		System.out.println("Recarga realizada. Novo saldo: " + this.saldo);
+	}
+
+ 	public void pagarViagem(double custo) {
+		// Regra de negócio: O custo deve ser positivo e o saldo deve ser suficiente
+
+ 		if (custo <= 0) {
+ 			System.out.println("Erro: O custo da viagem é inválido.");
+ 			return;
+ 		}
+		 if (this.saldo < custo) {
+ 			System.out.println("Erro: Saldo insuficiente para realizar a viagem.");
+ 			return;
+ 		}
+ 		this.saldo -= custo;
+ 			System.out.println("Viagem paga. Saldo restante: " + this.saldo);
+ 	}
+ }
