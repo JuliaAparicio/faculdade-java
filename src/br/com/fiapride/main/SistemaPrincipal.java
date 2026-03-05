77b454de7fa9a7b7c8f333fc7777276e361a4c11
Julia Aparicio <juliaaparicio756@gmail.com>
2026-03-04 21:33:07 -0300

    Update SistemaPrincipal.java

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,31 +1,33 @@
 package br.com.fiapride.main;
 
- // Importamos a classe Computador para que o sistema a reconheça
 import br.com.fiapride.model.Computador;
 
 public class SistemaPrincipal {
 
     public static void main(String[] args) {
-     // INSTANCIAÇÃO
-     // O comando 'new' aloca memória para um novo objeto.
-     // Criando o primeiro Computador (Objeto 1)
-    Computador computador1 = new Computador("Ana Silva", "222");
-     System.out.println("Regarga Computador 1");
-    Computador1.adicionarSaldo(50.0);
 
-     // Criando o segundo Computador (Objeto 2)
-     Computador computador2 = new Computador("Carlos Souza", "333");
-     System.out.println("Regarga Computador 2");
-     computador2.adicionarSaldo(12.5);
+        // Criando o primeiro Computador
+        Computador computador1 = new Computador(15.6, 512, 6000, 200);
 
-     System.out.println("--- Sistema FiapRide ---");
-    System.out.println("Computador: " + Computador1.nome + " | Saldo: R$ " + Computador1.saldo + " | CPF: " + Computador1.cpf);
-    System.out.println("Computador: " + Computador2.nome + " | Saldo: R$ " + Computador2.saldo + " | CPF: " + Computador2.cpf);
+        System.out.println("=== Computador 1 ===");
+        System.out.println("Tamanho: " + computador1.tamanho);
+        System.out.println("Memória: " + computador1.memoria + "GB");
+        System.out.println("Preço: R$ " + computador1.preco);
+        System.out.println("Saldo disponível: R$ " + computador1.saldo);
 
-    System.out.println("Pagando viagem do Computador 1");
-     computador1.pagarViagem(20);
-     System.out.println("Pagando viagem do Computador 2");
-     computador2.pagarViagem(20);
+        System.out.println("\nComprando jogo no Computador 1...");
+        computador1.comprarJogo(50);
 
+        // Criando o segundo Computador
+        Computador computador2 = new Computador(17.0, 1024, 6500, 30);
+
+        System.out.println("\n=== Computador 2 ===");
+        System.out.println("Tamanho: " + computador2.tamanho);
+        System.out.println("Memória: " + computador2.memoria + "GB");
+        System.out.println("Preço: R$ " + computador2.preco);
+        System.out.println("Saldo disponível: R$ " + computador2.saldo);
+
+        System.out.println("\nComprando jogo no Computador 2...");
+        computador2.comprarJogo(50);
     }
 }
